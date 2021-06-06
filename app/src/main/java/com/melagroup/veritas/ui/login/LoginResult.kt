13bbe17f1fff1217ae28e244d0270bca02798c51
talhatariqc232 <70269153+talhatariqc232@users.noreply.github.com>
2021-06-06 20:12:12 +0500

    Add files via upload

---
+++ app/src/main/java/com/melagroup/veritas/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.melagroup.veritas.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
